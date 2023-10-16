d3a03c5b4ba806010c204521a69c4acab32761b1
Alain Alberto <alainalberto03@gmail.com>
2023-10-16 18:02:54 -0500

    V10

--- funtion_extentions/models/partner.py
+++ funtion_extentions/models/partner.py
@@ -30,7 +30,7 @@ class ResPartner(models.Model):
      x_cliente_uin = fields.Char(string="UIN", tracking=True)
      x_cliente_parking_address = fields.Char(string="Parking Log Address", tracking=True)
      x_cliente_owner = fields.Char(string="Company Owner", tracking=True)
-     x_cliente_contact = fields.Many2many('res.reference', 'partner_id', 'Contacts', 'Reference to', string='Reference for', tracking=True)
+     x_cliente_contact = fields.Many2many('res.reference', string='Reference for', tracking=True)
      x_cliente_agent = fields.Many2many('res.users', string='Agent', domain="[('share', '=', False)]", tracking=True)
 
 
