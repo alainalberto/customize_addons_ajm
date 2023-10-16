7f729d9de64e84d3840f5431e8176e4b002f9278
Alain Alberto <alainalberto03@gmail.com>
2023-10-16 13:34:56 -0500

    Agrego de campos en respartner

--- funtion_extentions/models/partner.py
+++ funtion_extentions/models/partner.py
@@ -11,10 +11,14 @@ class ResPartner(models.Model):
      x_cliente_pin = fields.Char(string="US_DOT Pin", tracking=True)
      x_cliente_user = fields.Char(string="US_DOT User", tracking=True)
      x_cliente_pasw = fields.Char(string="US_DOT Password", tracking=True)
-     x_cliente_secut_quest = fields.Char(string="Security questions", tracking=True)
      x_cliente_mc = fields.Char(string="MC", tracking=True)
      x_cliente_tdmv = fields.Char(string="TDMV", tracking=True)
      x_cliente_login = fields.Char(string="Login", tracking=True)
      x_cliente_pass = fields.Char(string="Pass", tracking=True)
      x_cliente_uin = fields.Char(string="UIN", tracking=True)
-     x_cliente_taxid = fields.Char(string="TaxID", tracking=True)
+     x_cliente_parking_address = fields.Char(string="Parking Log Address", tracking=True)
+     x_cliente_owner = fields.Char(string="Company Owner", tracking=True)
+     x_ciente_contact = fields.Many2many('res.partner', string='Contact Referent', domain="[('is_company', '=', False)]")
+     x_cliente_agent = fields.Many2many('res.users', string='Agent to Working', domain="[('share', '=', False)]")
+     
+     
