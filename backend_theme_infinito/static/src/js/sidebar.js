3648b80bc0ae0468f46a9d38f04c2aac84b31751
Alain Alberto <alainalberto03@gmail.com>
2023-10-31 07:30:04 -0500

    Sidebar Menu Blocked

--- backend_theme_infinito/static/src/js/sidebar.js
+++ backend_theme_infinito/static/src/js/sidebar.js
@@ -2,8 +2,36 @@ odoo.define('sidebar_app.SidebarMenu', function (require) {
     "use strict";
 
     var session = require('web.session');
-
+     
     //sidebar toggle effect
+    $(document).on("load", function(event){
+        var sidebar = document.querySelector('#sidebar_panel')
+        if (sidebar.style.display == 'block') {
+            $("#openSidebar").hide();
+            $("#closeSidebar").show();
+            $("#sidebar_panel").css({'display':'block'});
+            let marginLeft = $("#sidebar_panel").css('width');
+            let margin_type = session.infinitoRtl ? 'margin-right' : 'margin-left';
+            let style = `${margin_type}: calc(${marginLeft} + 25px)`
+            $(".o_action_manager").css({'transition':'all .1s linear'});
+            $(".o_main_navbar").css({'transition':'all .1s linear'});
+            $(".o_action_manager").attr('style', style);
+
+            //add class in action-manager
+            var action_manager = $(".o_action_manager");
+            var action_manager_id = action_manager.data("id");
+            $("div").addClass(action_manager_id);
+            action_manager.addClass("sidebar_margin");
+
+            //add class in top_heading
+            var top_head = $(".top_heading");
+            var top_head_id = top_head.data("id");
+            $("div").addClass(top_head_id);
+            top_head.addClass("sidebar_margin");
+         } 
+         
+    });
+
     $(document).on("click", "#closeSidebar", function(event){
         $("#closeSidebar").hide();
         $("#openSidebar").show();
