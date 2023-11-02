5176b7ea990815bd3acb1893258669820f16e477
Alain Alberto <alainalberto03@gmail.com>
2023-11-02 16:50:47 -0500

    Style Module

--- backend_theme_infinito/static/src/js/sidebar.js
+++ backend_theme_infinito/static/src/js/sidebar.js
@@ -2,11 +2,8 @@ odoo.define('sidebar_app.SidebarMenu', function (require) {
     "use strict";
 
     var session = require('web.session');
-     
-    
-        
+
     //sidebar toggle effect
-    
     $(document).on("click", "#closeSidebar", function(event){
         $("#closeSidebar").hide();
         $("#openSidebar").show();
@@ -54,39 +51,31 @@ odoo.define('sidebar_app.SidebarMenu', function (require) {
     });
 
     $(document).on("click", ".sidebar a", function(event){
-        
-        if (window.innerWidth <= 992) {
-           
-           var menu = $(".sidebar a");
-           var $this = $(this);
-           var id = $this.data("id");
-           $("header").removeClass().addClass(id);
-           menu.removeClass("active");
-           $$(".o_action_manager").attr('style', style);
-           $("#closeSidebar").hide();
-           $("#openSidebar").show();this.addClass("active");
-           $("#sidebar_panel").css({'display':'none'});
-          
-           //sidebar close on menu-item click
-           let margin_type = session.infinitoRtl ? 'margin-right' : 'margin-left';
-           let style = `${margin_type}: 15px`;
-           var action_manager = $(".o_action_manager");
-           var action_manager_id = action_manager.data("id");
-           $("div").removeClass(action_manager_id);
-           action_manager.removeClass("sidebar_margin");
+        var menu = $(".sidebar a");
+        var $this = $(this);
+        var id = $this.data("id");
+        $("header").removeClass().addClass(id);
+        menu.removeClass("active");
+        $this.addClass("active");
 
-           //remove class in top_heading
-           var top_head = $(".top_heading");
-           var top_head_id = top_head.data("id");
-           $("div").removeClass(top_head_id);
-           top_head.removeClass("sidebar_margin");
-        } else {
-            $("#sidebar_panel").css({'display':'block'});
-        }
-        
+        //sidebar close on menu-item click
+        let margin_type = session.infinitoRtl ? 'margin-right' : 'margin-left';
+        let style = `${margin_type}: 15px`;
+        $("#sidebar_panel").css({'display':'none'});
+        $(".o_action_manager").attr('style', style);
+        $("#closeSidebar").hide();
+        $("#openSidebar").show();
 
         //remove class in action-manager
-        
+        var action_manager = $(".o_action_manager");
+        var action_manager_id = action_manager.data("id");
+        $("div").removeClass(action_manager_id);
+        action_manager.removeClass("sidebar_margin");
+
+        //remove class in top_heading
+        var top_head = $(".top_heading");
+        var top_head_id = top_head.data("id");
+        $("div").removeClass(top_head_id);
+        top_head.removeClass("sidebar_margin");
     });
-      
 });
