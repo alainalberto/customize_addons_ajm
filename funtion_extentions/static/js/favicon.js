87abfb8e26c153cf9eff94eb6c31b26db27fe0e8
Alain Alberto <alainalberto03@gmail.com>
2023-11-02 12:55:30 -0500

    Heade Name

--- funtion_extentions/static/js/favicon.js
+++ funtion_extentions/static/js/favicon.js
@@ -3,7 +3,7 @@
 import { WebClient } from "@web/webclient/webclient";
 import { patch } from "web.utils";
 
-patch(WebClient.prototype, "customize_title_header.WebClient", {
+patch(WebClient.prototype, "funtion_extentions.WebClient", {
     setup() {
         this._super();
         this.title.setParts({ zopenerp: "InsurAdmin" });
