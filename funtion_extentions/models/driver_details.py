4f84285ab9a4cafbbef93d1d53e5842c9299a83e
Alain Alberto <alainalberto03@gmail.com>
2023-11-07 21:47:40 -0600

    Error fields name

--- funtion_extentions/models/driver_details.py
+++ funtion_extentions/models/driver_details.py
@@ -7,7 +7,7 @@ from odoo.exceptions import UserError, ValidationError
 class DiverDetails(models.Model):
     _name = 'driver.details'
     
-    name = fields.Char(string="Name", related='resource_id.name', store=True, readonly=False, tracking=True)
+    name = fields.Char(string="Name", store=True, readonly=False, tracking=True)
     partner_id = fields.Many2one('res.partner', string='Customer')
     active = fields.Boolean('Active', related='resource_id.active', default=True, store=True, readonly=False)
     address_home_id = fields.Many2one(
