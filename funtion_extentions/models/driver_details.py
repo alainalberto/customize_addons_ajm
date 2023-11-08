b787f3da944744b28a3993adcf71e21869c78327
Alain Alberto <alainalberto03@gmail.com>
2023-11-07 22:34:00 -0600

    Driver model

--- funtion_extentions/models/driver_details.py
+++ funtion_extentions/models/driver_details.py
@@ -9,9 +9,9 @@ class DiverDetails(models.Model):
     
     name = fields.Char(string="Name", store=True, readonly=False, tracking=True)
     partner_id = fields.Many2one('res.partner', string='Customer')
-    active = fields.Boolean('Active', default=True, store=True, readonly=False)
+    active = fields.Boolean(string ='Active', default=True, store=True, readonly=False)
     address_home_id = fields.Text(string="Home Address")
     driving_license = fields.Binary(string="Driving License")
-    phone = fields.Char(related='address_home_id.phone', related_sudo=False, readonly=False, string="Private Phone")
+    phone = fields.Char( related_sudo=False, readonly=False, string="Private Phone")
     notes = fields.Text('Notes')
     
