4991ed6ec8c0adb3a6ef1f688c6aa0872032b7d8
Alain Alberto <alainalberto03@gmail.com>
2023-11-07 22:23:30 -0600

    Driver view address

--- funtion_extentions/models/driver_details.py
+++ funtion_extentions/models/driver_details.py
@@ -10,10 +10,7 @@ class DiverDetails(models.Model):
     name = fields.Char(string="Name", store=True, readonly=False, tracking=True)
     partner_id = fields.Many2one('res.partner', string='Customer')
     active = fields.Boolean('Active', default=True, store=True, readonly=False)
-    address_home_id = fields.Many2one(
-        'res.partner', 'Address', help='Enter here the private address of the driver, not the one linked to your company.',
-        tracking=True,
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    address_home_id = fields.Text(string="Home Address")
     driving_license = fields.Binary(string="Driving License")
     phone = fields.Char(related='address_home_id.phone', related_sudo=False, readonly=False, string="Private Phone")
     notes = fields.Text('Notes')
